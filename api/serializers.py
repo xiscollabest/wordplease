b85d5ddb9a2111a4b8dc46b2612e4186038fe68b
Carlos Guzmán <cguzman@xmltravelgate.com>
2019-02-28 21:02:22 +0100

    fix new user pass

--- api/serializers.py
+++ api/serializers.py
@@ -11,3 +11,9 @@ class UserSerializer(UserListSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'password', 'first_name', 'last_name', 'email')
+    
+    def create(self, validated_data):
+        user = super(UserSerializer, self).create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
